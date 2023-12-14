ea93deba34794e30659eff771e273136f6275663
slepoipupsik <murtazalieva.albina.04@mail.ru>
2023-12-14 15:57:03 +0300

    pract1

---
+++ pract1/src/Book.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+public class Book {
+    // Поля (атрибуты) класса
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    // Конструктор класса
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    // Методы доступа (геттеры и сеттеры) для полей
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String toString() {
+        return "Книга: " + title + ", Автор: " + author + ", Год издания: " + yearPublished;
+    }
+}
---
+++ pract1/src/testbook.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+public class testbook {
+    public static void main(String[] args) {
+        // Создаем объект класса Book
+        Book book1 = new Book("Программирование на Java", "Иван Иванов", 2023);
+
+        // Выводим информацию о книге
+        System.out.println(book1);
+    }
+}
